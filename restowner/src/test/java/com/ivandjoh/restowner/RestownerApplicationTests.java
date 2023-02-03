acf830a6d7a0a5cb38df5d9a1fb58fa3dc30502f
ivandjoh <ivandi.djoh@gmail.com>
2023-02-03 18:30:48 +0800

    first init

---
+++ restowner/src/test/java/com/ivandjoh/restowner/RestownerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivandjoh.restowner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestownerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
