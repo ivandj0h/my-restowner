9ee6a4edda7acc1b59457172ea51c14ceafae2b1
ivandjoh <ivandi.djoh@gmail.com>
2023-02-03 22:05:35 +0800

    init

--- restowner/src/main/java/com/ivandjoh/restowner/repository/InMemoryItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ivandjoh.restowner.repository;
-
-import com.ivandjoh.restowner.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InMemoryItemRepository extends JpaRepository<Item, Long> { }
